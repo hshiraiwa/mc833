220da9708887d4feaf064bae187c602be7331335
Luan <luanegidioferreira@gmail.com>
2017-11-27 22:38:42 -0200

    added file transfer code

--- chat/lib/client/file_transfer.c
+++ chat/lib/client/file_transfer.c
@@ -1,1 +1,30 @@
 #include "file_transfer.h"
+
+void writePoll(int sockfd, char* input){
+    struct pollfd pfd[1];
+    pfd->fd = sockfd;
+    pfd->events = POLLOUT;
+    int rv = poll(pfd, 1, TIMEOUT);
+
+    if(pfd[0].revents & POLLOUT) {
+        write(sockfd, input, strlen(input));
+        return;
+    }
+}
+
+void sendFile(int sockfd, char* fileName){
+	char* lineToSend = NULL;
+	size_t length = 0;
+
+	char filename[64];
+
+	FILE *fileToSend;
+	sprintf(filename, "%c.txt", fileName)
+	fileToSend = fopen(filename, "r");
+
+	while(getline(&lineToSend, &length, fileToSend) == -1){
+	    writePoll(sockfd, lineToSend);
+	}
+	fclose(fileToSend);
+}
+

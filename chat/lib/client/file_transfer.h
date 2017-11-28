029e0d3424829567c8c6fa3903947ef56c6dace5
Luan <luanegidioferreira@gmail.com>
2017-11-27 22:47:18 -0200

    new .h

--- chat/lib/client/file_transfer.h
+++ chat/lib/client/file_transfer.h
@@ -3,4 +3,8 @@
 
 #endif //FILE_TRANSFER_H
 
+//Writes input in a poll to the given socked
 void writePoll(int sockfd, char* input);
+
+//Sends a file with fileName to the given sockfd 
+void sendFile(int sockfd, char* fileName);
